83bf8712ef958bf73f31ae924946ac7bc5126ec8
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-07-16 05:57:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,101 +41,119 @@ $( document ).ready(function() {
         $('.card').animate({'opacity':'0.0'}, 1000, function (){
 
             //Utilizamos ajax para evitar que todo el proceso requiera de recargar la página.
-            $(".card").html(
-                '<div class="profile">'+
-                    '<img src="'+element.imgSplash+'" alt="">'+
-                    '<p>'+ element.nombrePJ+'</p>'+
-                    '</div>'+
-                    '<div class="materials">'+
-                        '<div class="level">'+
-                            '<p>Materiales de subida de nivel</p>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item1+'" alt="">'+
-                                '<p>x'+element.i1Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item2+'" alt="">'+
-                                '<p>x'+element.i2Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item3+'" alt="">'+
-                                '<p>x'+element.i3Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item4+'" alt="">'+
-                                '<p>x'+element.i4Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item5+'" alt="">'+
-                                '<p>x'+element.i5Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item6+'" alt="">'+
-                                '<p>x'+element.i6Cant+'</p>'+
-                            '</div>'+
-                        '</div>'+
-                        '<div class="talent">'+
-                            '<p>Materiales de Talentos</p>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item10+'" alt="">'+
-                                '<p>x'+element.i10Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item11+'" alt="">'+
-                                '<p>x'+element.i11Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item12+'" alt="">'+
-                                '<p>x'+element.i12Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item13+'" alt="">'+
-                                '<p>x'+element.i13Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item14+'" alt="">'+
-                                '<p>x'+element.i14Cant+'</p>'+
-                            '</div>'+
-                        '</div>'+
-                        '<div class="mixed">'+
-                            '<p>Materiales Mixtos</p>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item7+'" alt="">'+
-                                '<p>x'+element.i7Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item8+'" alt="">'+
-                                '<p>x'+element.i8Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item9+'" alt="">'+
-                                '<p>x'+element.i9Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item15+'" alt="">'+
-                                '<p>x'+element.i15Cant+'</p>'+
-                            '</div>'+
-                            '<div class="item">'+
-                                '<img src="'+element.item16+'" alt="">'+
-                                '<p>x'+element.i16Cant+'</p>'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>'
-            )
-            //La tarjeta, en un inicio oculta, pasará a ser flex-box para aparecer.
-            $('.card').css({'display':'flex'})
-            //Tendrá además una animación de aparición al cambiar su opacidad a 1.
-            $('.card').animate({'opacity':'1.0'}, 1000);
-            //El botón "crearImagen" pasará también a ser flex-box, apareciendo solo cuando aparezca la tarjeta.
-            $('#crearImagen').css({'display':'flex'})
-            /*Estas líneas generan una pequeña animación de desplazamiento hacia la tarjeta generada al pulsar en algún personaje.*/
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $(".card").offset().top
-            }, 1000);
+            $.ajax({
+                /*Utilizamos los datos del "selectPersonajes"*/ 
+                method : 'GET',
+                dataType: 'json',
+                url : 'pages/selectPersonajes.php',
+                data : {
+                    'cartaPersonaje' : cartaPersonaje
+                },
+
+                //Si lo anterior tiene éxito, traducirá los datos para convertirlos en un objeto que se pueda usar con jquery.
+                success:function(data){
+                     //A los datos, les aplicamos un bucle forEach para generar una tarjeta para cada personaje seleccionado.
+                     data.forEach(element => {
+                        //Ya que cada elemento de cada personaje tiene un color distinto, usamos de fondo una imagen distinta según el asignado en la BBDD.
+                        $('.card').css({'background-image':'url('+element.imgElem+')'})
+                        //Aquí modificamos el contenido del contenedor "card" y le añadimos todo el contenido de los personajes a la tarjeta.
+                        $(".card").html(
+                            '<div class="profile">'+
+                                '<img src="'+element.imgSplash+'" alt="">'+
+                                '<p>'+ element.nombrePJ+'</p>'+
+                                '</div>'+
+                                '<div class="materials">'+
+                                    '<div class="level">'+
+                                        '<p>Materiales de subida de nivel</p>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item1+'" alt="">'+
+                                            '<p>x'+element.i1Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item2+'" alt="">'+
+                                            '<p>x'+element.i2Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item3+'" alt="">'+
+                                            '<p>x'+element.i3Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item4+'" alt="">'+
+                                            '<p>x'+element.i4Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item5+'" alt="">'+
+                                            '<p>x'+element.i5Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item6+'" alt="">'+
+                                            '<p>x'+element.i6Cant+'</p>'+
+                                        '</div>'+
+                                    '</div>'+
+                                    '<div class="talent">'+
+                                        '<p>Materiales de Talentos</p>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item10+'" alt="">'+
+                                            '<p>x'+element.i10Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item11+'" alt="">'+
+                                            '<p>x'+element.i11Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item12+'" alt="">'+
+                                            '<p>x'+element.i12Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item13+'" alt="">'+
+                                            '<p>x'+element.i13Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item14+'" alt="">'+
+                                            '<p>x'+element.i14Cant+'</p>'+
+                                        '</div>'+
+                                    '</div>'+
+                                    '<div class="mixed">'+
+                                        '<p>Materiales Mixtos</p>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item7+'" alt="">'+
+                                            '<p>x'+element.i7Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item8+'" alt="">'+
+                                            '<p>x'+element.i8Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item9+'" alt="">'+
+                                            '<p>x'+element.i9Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item15+'" alt="">'+
+                                            '<p>x'+element.i15Cant+'</p>'+
+                                        '</div>'+
+                                        '<div class="item">'+
+                                            '<img src="'+element.item16+'" alt="">'+
+                                            '<p>x'+element.i16Cant+'</p>'+
+                                        '</div>'+
+                                    '</div>'+
+                                '</div>'
+                        )
+                        //La tarjeta, en un inicio oculta, pasará a ser flex-box para aparecer.
+                        $('.card').css({'display':'flex'})
+                        //Tendrá además una animación de aparición al cambiar su opacidad a 1.
+                        $('.card').animate({'opacity':'1.0'}, 1000);
+                        //El botón "crearImagen" pasará también a ser flex-box, apareciendo solo cuando aparezca la tarjeta.
+                        $('#crearImagen').css({'display':'flex'})
+                        /*Estas líneas generan una pequeña animación de desplazamiento hacia la tarjeta generada al pulsar en algún personaje.*/
+                        $([document.documentElement, document.body]).animate({
+                            scrollTop: $(".card").offset().top
+                        }, 1000);
+                    })
+                }
+            }); 
 
         });
         
     });
 
 });
-
