d6dae95b5863364a18eabef74240336eb042dcb0
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-07-16 06:30:25 +0200

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,5 +1,5 @@
 <?php
     /*Primero incluimos la página "pdoconfig.php" para poder utilizar sus datos y conectarnos así a nuestra BBDD.*/
     require "pdoconfig.php";
-    $connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password, $options);
+    $connection = new PDO("mysql:host=".$host.";dbname=".$dbname,$username,$password,$options);
 ?>
