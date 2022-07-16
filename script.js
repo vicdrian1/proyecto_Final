7fab5f33f7eefc97cd9864dbf78dde70effd612b
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-07-16 05:47:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ $( document ).ready(function() {
                 //Si lo anterior tiene éxito, traducirá los datos para convertirlos en un objeto que se pueda usar con jquery.
                 success:function(data){
                     console.log(data)
-                     data = JSON.parse(data);
                      //A los datos, les aplicamos un bucle forEach para generar una tarjeta para cada personaje seleccionado.
                      data.forEach(element => {
                         //Ya que cada elemento de cada personaje tiene un color distinto, usamos de fondo una imagen distinta según el asignado en la BBDD.
