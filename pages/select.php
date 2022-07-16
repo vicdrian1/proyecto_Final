e5ab1a40f8d4c82f89c4ebc44aa06871358310fa
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-07-16 04:21:18 +0200

    Update select.php

--- pages/select.php
+++ pages/select.php
@@ -1,8 +1,6 @@
 <?php
 //Con estos datos obtenemos TODOS los datos de la tabla "personajes" de nuestra BBDD. Además, los ordenamos por orden alfabético según el nombre.
 try{
-    include "../db/pdoconfig.php";
-    $connection = new PDO("mysql: host=".$host."; dbname=".$dbname, $username, $password, $options);
     $ids = $connection->query("SELECT * FROM personajes ORDER BY personajes.nombre" )->fetchAll();
 }catch(Exception $error){
     echo "<p>Lo sentimos, algo ha fallado.</p>";
