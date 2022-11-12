e9feae0f6335e5f439b7c607a3dea9a709335fb2
vicdrian1 <104452446+vicdrian1@users.noreply.github.com>
2022-11-12 15:03:26 +0100

    Update pdoconfig.php

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -11,4 +11,5 @@
         \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE  => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES  => false
-    ]; ?>
+    ];
+?>
